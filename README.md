184ac855a098c6b2bfc985c35f149392283a0184
Maxim <maxtab1@mail.ru>
2024-08-07 10:44:05 +0300

    regular

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
+# Yandex test
+
 Without feedback from the designer, it is difficult to achieve a pixel-perfect layout. A lot of extra time is wasted.
--- js/main.js
+++ js/main.js
@@ -1,2 +1,9 @@
 const { log } = console;
 log("hello");
+
+window.onload = () => {
+	test.innerText = main_section_1__id.offsetWidth;
+};
+window.onresize = () => {
+	test.innerText = main_section_1__id.offsetWidth;
+};
